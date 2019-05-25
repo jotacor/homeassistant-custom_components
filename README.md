df9fc1e9563351ad684d75d71db74a04c048ab47
Javier Corbín <javi.corbin@gmail.com>
2019-05-25 02:36:49 +0200

    First version for Movistar Askey RFT3505 Fiber Router device tracker for Home Assistant

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# homeassistant-custom_components
+# Home Assistant custom components
 My own custom components for Home Assistant
+
+## Askey RFT3505 Fiber Router (Spain) - Device Tracker
+### Configuration variables:
+**host**: The hostname or IP of your router.<br/>
+**password**: The password of your router.<br/>
+
+### Installation:
+1. Download and unzip all the content from _askey_rft3505_ directory to your `/config/custom_components/askey_rft3505/` directory.
+2. Add the configuration to the `configuration.yaml`:
+```
+    device_tracker:
+      - platform: askey_rft3505
+        host: ROUTER_IP
+        password: YOUR_PASSWORD
+        interval_seconds: 120
+        consider_home: 200
+        new_device_defaults:
+          track_new_devices: False
+          hide_if_away: False
+```
+3. Restart the Home Assistant server.
---
+++ askey_rft3505/device_tracker.py
@@ -0,0 +1,88 @@
+"""Support for Askey RFT3505 Router (Movistar Spain)"""
+import base64
+from datetime import datetime
+import hashlib
+import logging
+import re
+import requests
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.device_tracker import (DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import (CONF_HOST, CONF_PASSWORD, HTTP_HEADER_X_REQUESTED_WITH)
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string
+})
+
+
+def get_scanner(hass, config):
+    """Validate the configuration and return a scanner."""
+
+    scanner = AskeyDeviceScanner(config[DOMAIN])
+    return scanner if scanner.success_init else None
+
+
+class AskeyDeviceScanner(DeviceScanner):
+    """This class queries a Askey RFT3505 Fiber Router (Movistar Spain)"""
+
+    def __init__(self, config):
+        """Initialize the scanner."""
+        self.host = config[CONF_HOST]
+        self.password = config[CONF_PASSWORD]
+
+        self.parse_macs = re.compile(r'([0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2}:' + '[0-9a-fA-F]{2})')
+
+        self.login_url = 'http://{ip}/te_acceso_router.cgi'.format(**{'ip': self.host})
+
+        self.last_results = {}
+        self.success_init = self._update_info()
+
+    def scan_devices(self):
+        """Scan for new devices and return a list with found device IDs."""
+        self._update_info()
+        return self.last_results
+
+
+    def get_device_name(self, device):
+        """This router doesn't save the name of the wireless device."""
+        return None
+
+    def _update_info(self):
+        """Ensure the information from the router is up to date.
+        Return boolean if scanning successful.
+        """
+        _LOGGER.info('Checking Router')
+
+        data = self.get_askey_info()
+        if not data:
+            return False
+
+        self.last_results = data
+        return True
+
+    def get_askey_info(self):
+        """Retrieve data from router."""
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:67.0) Gecko/20100101 Firefox/67.0',
+                   'Cookie': 'sessionID=123456789'}
+        data = {'loginPassword': self.password}
+
+        session = requests.Session()
+        login_response = session.post(self.login_url, data=data, headers=headers)
+
+        if login_response.status_code == 200:
+            url = 'http://{}/te_mapa_red_local.html'.format(self.host)
+            response = session.get(url, headers=headers, timeout=4)
+            response_string = str(response.content)
+            parsed_macs = self.parse_macs.findall(response_string)
+            result = [ mac.replace(" ", "_") for mac in parsed_macs ]
+        else:
+            result = None
+            _LOGGER.info('Error connecting to the router...')
+
+        session.close()
+        return result
